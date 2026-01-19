58c5fa38bb7fda92c003fe5c7e4cbbc8598e9b95
kevinggundersen <kevingg@uia.no>
2026-01-19 15:10:24 +0100

    feat:
    add: "Quick Outline" asset from Unity AssetStore
    add: HoverEffect script  to enable outline when interactible is hovered with ray interactor

---
+++ Simple digital twin/Assets/Scripts/HoverEffect.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables; 
+
+[RequireComponent(typeof(XRSimpleInteractable))]
+public class HoverHighlight : MonoBehaviour
+{
+    private XRSimpleInteractable _interactable;
+    private Outline _outline;
+
+    void Awake()
+    {
+        _interactable = GetComponent<XRSimpleInteractable>();
+        _outline = GetComponent<Outline>();
+
+        // Ensure outline is off at the start
+        if (_outline != null) _outline.enabled = false;
+    }
+
+    void OnEnable()
+    {
+        _interactable.hoverEntered.AddListener(OnHoverEnter);
+        _interactable.hoverExited.AddListener(OnHoverExit);
+    }
+
+    void OnDisable()
+    {
+        _interactable.hoverEntered.RemoveListener(OnHoverEnter);
+        _interactable.hoverExited.RemoveListener(OnHoverExit);
+    }
+
+    private void OnHoverEnter(HoverEnterEventArgs args)
+    {
+        if (_outline != null) _outline.enabled = true;
+    }
+
+    private void OnHoverExit(HoverExitEventArgs args)
+    {
+        if (_outline != null) _outline.enabled = false;
+    }
+}
