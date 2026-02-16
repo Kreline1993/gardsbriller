4b07d97c221adb8b0cbcb6a74c57cae0a3b9f051
kevinggundersen <159529468+kevinggundersen@users.noreply.github.com>
2026-02-16 09:19:04 +0100

    feat: only spawn digital twin on specific qr code (#22)
    Only spawns the digital twin if a QR code with ID QR000 is detected

--- Simple digital twin/Assets/Scripts/QRCodePlacementByManualOffset.cs
+++ Simple digital twin/Assets/Scripts/QRCodePlacementByManualOffset.cs
@@ -18,19 +18,28 @@ public class QRCodePlacementByManualOffset : MonoBehaviour
 
         if (spawnedPrefabs.ContainsKey(trackable))
             return;
+        string QRContent = GetQRCodeContent(trackable);
+        if (QRContent == "QR000")
+        {
+            Debug.Log("QR code with ID QR000 detected. Spawning QR digital twin...");
+            // Calculate spawn position with offset
+            Vector3 spawnPosition = trackable.transform.position + trackable.transform.TransformDirection(spawnOffset);
+            
+            GameObject go = Instantiate(
+                qrPrefab,
+                spawnPosition,
+                trackable.transform.rotation
+            );
 
-        // Calculate spawn position with offset
-        Vector3 spawnPosition = trackable.transform.position + trackable.transform.TransformDirection(spawnOffset);
-        
-        GameObject go = Instantiate(
-            qrPrefab,
-            spawnPosition,
-            trackable.transform.rotation
-        );
+            //go.transform.SetParent(trackable.transform, worldPositionStays: true); // Comment this out to keep the prefab at its initial position
 
-        //go.transform.SetParent(trackable.transform, worldPositionStays: true); // Comment this out to keep the prefab at its initial position
+            spawnedPrefabs.Add(trackable, go);
+        }
+        else
+        {
+            Debug.Log("QR code with ID " + QRContent + " detected. Not spawning QR digital twin.");
+        }
 
-        spawnedPrefabs.Add(trackable, go);
     }
 
     public void OnTrackableRemoved(MRUKTrackable trackable)
@@ -40,5 +49,14 @@ public class QRCodePlacementByManualOffset : MonoBehaviour
 
         Destroy(go);
         spawnedPrefabs.Remove(trackable);
+    }
+
+    public string GetQRCodeContent(MRUKTrackable trackable)
+    {
+        // Get QR code content based on the trackable's properties
+        Debug.Log("Getting QR code content for trackable: " + trackable.GetType().Name);
+        var QRContent = trackable.MarkerPayloadString;
+        Debug.Log("Trackable QR ID: " + QRContent);
+        return QRContent; // Return just the payload string, not a formatted string
     }
 }
